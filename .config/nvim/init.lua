91798c04c5a2725903b6fa64cef79ed5f0c977a6
Mike Ulicny <MichaelAUlicny@gmail.com>
2024-08-18 23:20:38 -0400

    move packer bootstrap to top of config file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,18 @@
+-- Plugin manager, will bootstrap packer install if not already installed
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+require('plugins')
+
 -- No swap file recovery
 vim.opt.swapfile = false
 
@@ -53,21 +68,6 @@ vim.filetype.add({
         templ = "templ",
     },
 })
-
--- Plugin manager, will bootstrap packer install if not already installed
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
-end
-
-local packer_bootstrap = ensure_packer()
-require('plugins')
 
 -- Start LuaLine
 require('lualine').setup{}
