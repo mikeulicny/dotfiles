be4d9e0c1108f10b5e1ec6ad06f4d8ab5e3455df
Mike Ulicny <MichaelAUlicny@gmail.com>
2024-10-14 19:24:01 -0400

    update config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -23,11 +23,12 @@ return {
     lazy = false,
     config = function()
       require('mason-lspconfig').setup({
+        --ensure_installed = {'lua_ls', 'rust_analyzer', 'gopls', 'clangd'},
         automatic_installation = true,
         handlers = {
           function(server_name)
             require('lspconfig')[server_name].setup({})
-          end
+          end,
         },
       })
     end,
