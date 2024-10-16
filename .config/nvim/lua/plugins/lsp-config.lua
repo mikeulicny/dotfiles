de85d3aee131aaa8cba1a5d5519f4b55025a6541
Mike Ulicny <MichaelAUlicny@gmail.com>
2024-10-15 20:08:06 -0400

    update config to correctly load lsp configuration

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -3,19 +3,14 @@ return {
     'neovim/nvim-lspconfig',
     config = function()
       vim.opt.signcolumn = 'yes'
-      local lspconfig_defaults = require('lspconfig').util.default_config
-      lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-        'force',
-        lspconfig_defaults.capabilities,
-        require('cmp_nvim_lsp').default_capabilities()
-      )
+      require('mason').setup()
+      require('mason-lspconfig').setup()
     end
   },
   {
     'williamboman/mason.nvim',
     lazy = false,
     config = function()
-      require('mason').setup()
     end
   },
   {
@@ -23,7 +18,7 @@ return {
     lazy = false,
     config = function()
       require('mason-lspconfig').setup({
-        --ensure_installed = {'lua_ls', 'rust_analyzer', 'gopls', 'clangd'},
+        ensure_installed = {'lua_ls', 'rust_analyzer', 'gopls', 'clangd'},
         automatic_installation = true,
         handlers = {
           function(server_name)
