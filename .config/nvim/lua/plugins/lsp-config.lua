d4e0ca378733ff3fe254b262bbe87d7850c3e096
Mike Ulicny <MichaelAUlicny@gmail.com>
2024-10-15 22:38:59 -0400

    temporary fix to lsp config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -10,19 +10,19 @@ return {
     'williamboman/mason-lspconfig.nvim',
     dependencies = { 'williamboman/mason.nvim' },
     lazy = false,
-    config = function()
-      require('mason-lspconfig').setup({
-        automatic_installation = true,
-        handlers = {
-          function(server_name)
-            require('lspconfig')[server_name].setup({})
-          end,
-        },
-      })
-    end,
+    opts = {
+      automatic_installation = true,
+      handlers = {
+        function (server_name)
+          require('lspconfig')[server_name].setup({})
+        end,
+      }
+      
+    },
   },
   {
     'neovim/nvim-lspconfig',
+    version = "v1.0.0",
     config = function()
       vim.opt.signcolumn = 'yes'
     end,
