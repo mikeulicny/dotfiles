7a2016e505dacde3541ef19d2edf9a4ee228929e
Mike Ulicny <MichaelAUlicny@gmail.com>
2026-06-13 14:01:37 -0400

    fix/update hyprland window rules for good floating tiles. Update background to cycle Nous Research images

--- .config/hypr/rules.lua
+++ .config/hypr/rules.lua
@@ -1,4 +1,33 @@
-hl.window_rule({
+hl.layer_rule({
     name = "vicinae-blur",
     match = { namespace = "vicinae" },
+    blur = true,
+    ignore_alpha = 0
+})
+
+hl.layer_rule({
+    name = "vicinae-no-animation",
+    match = { class = "vicinae" },
+    no_anim = true
+})
+
+hl.window_rule({
+    name = "1Password-float",
+    match = { class = "1Password" },
+    float = true,
+    center = true
+})
+
+hl.window_rule({
+    name = "files-float",
+    match = { class = "org.gnome.Nautilus" },
+    float = true,
+    center = true
+})
+
+hl.window_rule({
+    name = "float-file-pickers",
+    match = { title = "Open.*Files?|Save.*Files?|Open.*Folders?|All Files|Save" },
+    float = true,
+    center = true
 })
